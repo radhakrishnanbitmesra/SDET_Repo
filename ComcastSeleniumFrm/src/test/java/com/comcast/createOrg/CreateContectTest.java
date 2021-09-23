924715895fad908ff973adb55534326015c0062e
radhakrishnanbitmesra <radhakrishnanbitmesra@gmail.com>
2021-09-23 12:23:21 +0530

    first code

---
+++ ComcastSeleniumFrm/src/test/java/com/comcast/createOrg/CreateContectTest.java
@@ -0,0 +1,24 @@
+package com.comcast.createOrg;
+
+import org.testng.annotations.Test;
+
+public class CreateContectTest {
+	
+	@Test
+	public void creatContectTest()
+	{
+		System.out.println("Execute CreateContectTest ");
+	}
+	
+	@Test
+	public void creatContectWithOrgTest()
+	{
+		System.out.println("Execute creatContectWithOrgTest ");
+	}
+	
+	@Test
+	public void deleatContectTest()
+	{
+		System.out.println("Execute deleatContectTest ");
+	}
+}
---
+++ ComcastSeleniumFrm/src/test/java/com/comcast/orgTest/OrgTest.java
@@ -0,0 +1,25 @@
+package com.comcast.orgTest;
+
+import org.testng.annotations.Test;
+
+public class OrgTest {
+	
+	@Test
+	public void creatOrgTest()
+	{
+		System.out.println("Execute creatOrgTest ");
+	}
+	
+	@Test
+	public void creatContectOrgTest()
+	{
+		System.out.println("Execute creatContectOrgTest ");
+	}
+	
+	@Test
+	public void deleatOrgTest()
+	{
+		System.out.println("Execute deleatOrgTest ");
+	}
+
+}
